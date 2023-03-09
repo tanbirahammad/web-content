7f50b1015f0ef972986b3251e32b6b2a58b4fa42
tanbirahammad <jht017933@gmail.com>
2023-03-09 02:04:35 -0500

    firebase env added

--- src/Firebase.config.js
+++ src/Firebase.config.js
@@ -1,10 +1,13 @@
+import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-    apiKey: "AIzaSyCMmQW-EReT9hypUUuVCdyFzY-8Q4tTsgg",
-    authDomain: "artistic-communication.firebaseapp.com",
-    projectId: "artistic-communication",
-    storageBucket: "artistic-communication.appspot.com",
-    messagingSenderId: "108634737900",
-    appId: "1:108634737900:web:86380c050a7233a20629c9"
+    apiKey:process.env.REACT_APP_API_KEY,
+    authDomain:process.env.REACT_APP_AUTH_DOMAIN ,
+    projectId:process.env.REACT_APP_PROJECT_ID ,
+    storageBucket:process.env.REACT_APP_STORAGE_BUCKET ,
+    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId:process.env.REACT_APP_APP_ID 
   };
 
-export default firebaseConfig;
+  const app = initializeApp(firebaseConfig);
+
+  export default app;
