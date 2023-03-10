ac01da610529ef49db7558c965322a43a8c94a96
Jonymia3021 <88202620+Jonymia3021@users.noreply.github.com>
2023-03-10 21:08:47 +0600

    jony

--- src/Components/dashboard/pages/e_tender/Etender.jsx
+++ src/Components/dashboard/pages/e_tender/Etender.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './etender.css';
+import img from '../../../../images/icons/e-tender.png';
 
 const EtenderDash = () => {
+
+    const [toggleState, setToggleState] = useState('seeAll');
+
+    const handleToggleTender = (e) => {
+        setToggleState(e.target.value)
+    }
     return (
-        <div>
-            e tender
+        <div id='e-tender-dash' className='row'>
+            <h4>E Tender</h4>
+            <div className="tender-toggle-box">
+                <button onClick={handleToggleTender} value='seeAll' className="btn btn-outline-dark">See All Tender</button>
+                <input style={{width:'50%'}} type="text" className='form-control' placeholder='Search...' />
+                <button onClick={handleToggleTender} value='newTender' className="btn btn-outline-dark" >Create New Tender</button>
+            </div>
+            {toggleState === 'seeAll' && <div className="tender-list-container">
+                <div className="tender-list shadow-sm">
+                    <img src={img} alt="" />
+                    <h6>hhhhhhhhh</h6>
+                    <div className="tender-list-button">
+                        <button className='btn btn-info'>Edit</button>
+                        <button className='btn btn-danger'><i className="bi bi-x-circle"></i></button>
+                    </div>
+                </div>
+            </div>}
+            {toggleState === 'newTender' && <div className='toogle-newTender-container shadow-sm'>
+                <h4>Create new tender</h4>
+                <form>
+                    <label className='form-label' htmlFor="title">Title</label>
+                    <input className='form-control bg-grey' type="text" name='title' />
+                    <br />
+                    <label className='form-label' htmlFor="formFile">Choose an image</label>
+                    <input className="form-control bg-grey" type="file" name="formFile"></input>
+                    <br />
+                    <label htmlFor="body">Tender body</label>
+                    <textarea name="body" className='form-control bg-grey'></textarea>
+                    <br />
+                    <input className='btn btn-dark' type="submit" value="Create" />
+                </form>
+            </div> }
         </div>
     );
 };
--- src/Components/dashboard/pages/partner/Partner.jsx
+++ src/Components/dashboard/pages/partner/Partner.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './partner.css';
 
 const PartnerDash = () => {
+
+    const [partnerInfo, setPartnerInfo] = useState('list');
+
+    const handlePartnerInfo = (e) => {
+        setPartnerInfo(e.target.value);
+    }
+
     return (
-        <div>
-            partner
+        <div className="row" id="partnerForm">
+            <h5>All Partner Applicants List</h5>
+            
+                {partnerInfo === 'list' && <div className="partner-card-container">
+                <div className="input-group mb-3 search-group">
+                    <input type="text" className="form-control" placeholder="search..." aria-label="Username" aria-describedby="basic-addon1" />
+                    <span className="input-group-text btn btn-secondary" id="basic-addon1"><i className="bi bi-search"></i></span>
+                </div>
+                    <div className="card shadow-sm">
+                        <div className="card-body">
+                            <h5 className="card-title">Name</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">Email</h6>
+                            <div>
+                                <button onClick={handlePartnerInfo} value='info' className='card-link btn btn-info'>See Details</button>
+                                <button className='card-link btn btn-danger'>Delete</button>
+                            </div>
+                        </div>
+                    </div>  
+                </div>}
+                {partnerInfo === 'info' && <div className="partnerFormDetails">
+                    <div>
+                        <button onClick={handlePartnerInfo} value='list' className='btn btn-outline-dark'><i className="bi bi-arrow-left"></i> Return to Partner List</button>
+                    </div>
+                    <div className='partnerInfo'>
+                        <h6>Name</h6>
+                        <h6>Email</h6>
+                        <h6>Phone</h6>
+                        <h6>Hear about us</h6>
+                        <h6>Company Name</h6>
+                        <h6>Business Type</h6>
+                        <h6>Address</h6>
+                        <h6>Website</h6>
+                    </div>
+                </div>}
         </div>
     );
 };
